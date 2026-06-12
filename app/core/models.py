2ff81ca5221a1e8167d1598b18d154a6a201cc97
Bright Djogatse <brightdjogatse@gmail.com>
2026-06-12 10:41:02 +0000

    added features for user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,38 @@
+"""Database models"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """User Manager"""
+
+    def create_user(self, email, password=None, **extrafields):
+        """create, save and return a new user"""
+        email = self.normalize_email(email)
+        if not email:
+            raise ValueError('An email is required to log in')
+        user = self.model(email=email, **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
